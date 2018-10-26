eb0afba3c9dfdf7af16002cc2e22d26fe3d7aa72
AxcLogo <axclogo@163.com>
2018-10-26 17:14:05 +0800

    移除没用的包

--- Podfile
+++ Podfile
@@ -5,8 +5,5 @@ inhibit_all_warnings!
 target “AxcDrawPath_Tool” do
 
    pod 'Masonry'
-   pod 'MJRefresh'
-   pod 'AFNetworking', '~> 3.1.0'
-   pod 'SDWebImage', '~> 3.8.2'
 
 end
--- Pods/Target Support Files/Pods-AxcDrawPath_Tool/Pods-AxcDrawPath_Tool-resources.sh
+++ Pods/Target Support Files/Pods-AxcDrawPath_Tool/Pods-AxcDrawPath_Tool-resources.sh
@@ -91,12 +91,6 @@ EOM
       ;;
   esac
 }
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
-fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
